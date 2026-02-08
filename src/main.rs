2efd216c09fafe8a0245fb5526a28302f40c31b5
Samuel Åkesson <samak519@student.liu.se>
2026-02-08 11:11:28 +0100

    working http serve

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,139 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
+#![feature(impl_trait_in_assoc_type)]
+#![feature(type_alias_impl_trait)]
 
+use core::net::Ipv4Addr;
+
+use dotenvy_macro::dotenv;
 use embassy_executor::Spawner;
-use esp_hal::delay::Delay;
-use esp_hal::gpio::{Input, InputConfig, Pull};
-use esp_hal::rmt::Rmt;
-use esp_hal::time::Rate;
-use esp_hal::tsens::TemperatureSensor;
-use esp_hal::{main, tsens};
-use esp_hal_smartled::{RmtSmartLeds, Ws2812Timing, buffer_size, color_order};
+use embassy_net::{Runner, StackResources};
+use embassy_time::{Duration, Timer};
+use esp_alloc as _;
+use esp_backtrace as _;
+use esp_hal::interrupt::software::SoftwareInterruptControl;
+use esp_hal::ram;
+use esp_hal::{clock::CpuClock, rng::Rng, timer::timg::TimerGroup};
 use esp_println::println;
-use smart_leds::hsv::{Hsv, hsv2rgb};
-use smart_leds::{RGB8, SmartLedsWrite, brightness, gamma};
+use esp_radio::{
+    Controller,
+    wifi::{ClientConfig, ModeConfig, WifiController, WifiDevice, WifiEvent, WifiStaState},
+};
+use picoserve::{AppBuilder, AppRouter, make_static, routing::get};
+
+const IPV4_ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 220);
+const SSID: &str = dotenv!("SSID");
+const PASSWORD: &str = dotenv!("PASSWORD");
+static PICOSERVE_CONFIG: picoserve::Config =
+    picoserve::Config::const_default().keep_connection_alive();
+
+struct AppProps;
+
+impl AppBuilder for AppProps {
+    type PathRouter = impl picoserve::routing::PathRouter;
+
+    fn build_app(self) -> picoserve::Router<Self::PathRouter> {
+        picoserve::Router::new().route("/", get(|| async move { "Hello World" }))
+    }
+}
+const WEB_TASK_POOL_SIZE: usize = 1; // FIXME: what's possible?
+#[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
+async fn web_task(
+    task_id: usize,
+    stack: embassy_net::Stack<'static>,
+    app: &'static AppRouter<AppProps>,
+) -> ! {
+    let port = 80;
+    let mut tcp_rx_buffer = [0; 1024];
+    let mut tcp_tx_buffer = [0; 1024];
+    let mut http_buffer = [0; 2048];
+
+    picoserve::Server::new(app, &PICOSERVE_CONFIG, &mut http_buffer)
+        .listen_and_serve(task_id, stack, port, &mut tcp_rx_buffer, &mut tcp_tx_buffer)
+        .await
+        .into_never()
+}
 
 esp_bootloader_esp_idf::esp_app_desc!();
-
-
 #[esp_rtos::main]
 async fn main(spawner: Spawner) {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
+    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::_80MHz);
+    let peripherals = esp_hal::init(config);
 
-    let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
-    type LedColor = RGB8;
-    let mut led = RmtSmartLeds::<
-        { buffer_size::<LedColor>(1) },
-        _,
-        LedColor,
-        color_order::Rgb,
-        Ws2812Timing,
-    >::new_with_memsize(rmt.channel0, peripherals.GPIO10, 2)
-    .unwrap();
+    esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
+    esp_alloc::heap_allocator!(size: 36 * 1024);
 
-    let button = Input::new(
-        peripherals.GPIO9,
-        InputConfig::default().with_pull(Pull::Up),
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_int = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
+
+    let esp_radio_ctrl = make_static!(Controller, esp_radio::init().unwrap());
+
+    let (controller, interfaces) =
+        esp_radio::wifi::new(esp_radio_ctrl, peripherals.WIFI, Default::default()).unwrap();
+    spawner.must_spawn(wifi_task(controller));
+
+    let wifi_interface = interfaces.sta;
+
+    let rng = Rng::new();
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+
+    let (stack, runner) = embassy_net::new(
+        wifi_interface,
+        embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+            address: embassy_net::Ipv4Cidr::new(IPV4_ADDR, 24),
+            gateway: Some(Ipv4Addr::new(192, 168, 1, 1)),
+            dns_servers: Default::default(),
+        }),
+        make_static!(StackResources<WEB_TASK_POOL_SIZE>, StackResources::new()),
+        seed,
     );
+    spawner.must_spawn(net_task(runner));
 
-    let delay = Delay::new();
+    let app = make_static!(AppRouter<AppProps>, AppProps.build_app());
 
-    let mut color = Hsv {
-        hue: 0,
-        sat: 255,
-        val: 255,
-    };
-    let mut color_data;
-
-    let temperature_sensor =
-        TemperatureSensor::new(peripherals.TSENS, tsens::Config::default()).unwrap();
-
-    loop {
-        if button.is_low() {
-            color.hue = color.hue.wrapping_add(1);
-            color_data = [hsv2rgb(color)];
-            led.write(brightness(gamma(color_data.iter().cloned()), 10))
-                .unwrap();
-        }
-
-        println!(
-            "temp: {:?}",
-            temperature_sensor.get_temperature().to_celsius()
-        );
-        delay.delay_millis(100);
+    for i in 0..WEB_TASK_POOL_SIZE {
+        spawner.must_spawn(web_task(i, stack, app));
     }
 }
 
-#[panic_handler]
-fn panic(panic_info: &core::panic::PanicInfo) -> ! {
-    println!("PANICKED: {panic_info}");
-    loop {}
+#[embassy_executor::task]
+async fn wifi_task(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        if esp_radio::wifi::sta_state() == WifiStaState::Connected {
+            controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            Timer::after(Duration::from_millis(5000)).await
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = ModeConfig::Client(
+                ClientConfig::default()
+                    .with_ssid(SSID.into())
+                    .with_password(PASSWORD.into()),
+            );
+            controller.set_config(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start_async().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => {
+                println!("Wifi connected!");
+                println!("visit http://{}", IPV4_ADDR);
+            }
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn net_task(mut runner: Runner<'static, WifiDevice<'static>>) {
+    runner.run().await
 }
